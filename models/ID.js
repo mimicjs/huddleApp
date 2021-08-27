6292edd3058ca1e82fba3c996778593753d2553f
Jacky <32786237+mimicjs@users.noreply.github.com>
2021-08-28 10:46:04 +1200

    Delete ID.js

--- models/ID.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ID = void 0;
-var mongoose_1 = require("mongoose");
-exports.ID = {
-    type: mongoose_1.Schema.Types.ObjectId,
-    ref: ''
-};
